062b57ec33a8ce3086bf1ab5200a24d236d94986
dgrah50 <d.graham50@hotmail.co.uk>
2019-05-17 15:50:52 +0100

    load users from firebase

---
+++ web/scripts/userfetch.js
@@ -0,0 +1,43 @@
+var config = {
+    apiKey: "AIzaSyB6mXwNEirNupF2wT28lclPJ9YjvFe1eQo",
+    authDomain: "mfmonitor-80a0d.firebaseapp.com",
+    databaseURL: "https://mfmonitor-80a0d.firebaseio.com",
+    projectId: "mfmonitor-80a0d",
+    storageBucket: "mfmonitor-80a0d.appspot.com",
+    messagingSenderId: "171419436747",
+    appId: "1:171419436747:web:e45ee688cbb5c5f0"
+};
+firebase.initializeApp(config);
+
+function generateHTML(patientInfo, userID) {
+    return `
+<div class="patient_block">
+                <img src="images/profile_pic.png" class="profile_pic">
+                <div class="patient_id">
+                    <ul>
+                        <li><b>Name: </b>${patientInfo.Name}</li>
+                        <li><b>DOB: </b>${patientInfo.DOB}</li>
+                    </ul>
+                    <p id="demo"></p>
+                </div>
+                <div class="patient_status">
+                    <ul>
+                        <li><b>Abnormality detected: </b>${patientInfo.Abnormality}</li>
+                        <li><b>Condition: </b>${patientInfo.Condition} </li>
+                        <li><b>Connection status: </b>XXX</li>
+                    </ul>
+                 </div>
+                 <div class="unstarred"></div>
+                 <div class = "view_btn"><a href="live_patient.html">VIEW</a></div>
+            </div>
+            `
+}
+let ref = firebase.database().ref("/")
+ref.on("value", function (snapshot) {
+    for (var userID in snapshot.val()) {
+        let patientInfo = snapshot.val()[userID]['patientInfo']
+        document.getElementById("patientlist").innerHTML += generateHTML(patientInfo, userID);
+    }
+}, function (errorObject) {
+    console.log("The read failed: " + errorObject.code);
+});
