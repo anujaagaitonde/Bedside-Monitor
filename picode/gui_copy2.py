dcc74104595cab5d09ae57c8f78dd93f95ff9f0a
anujaagaitonde <anujaagaitonde@gmail.com>
2019-06-11 20:29:49 +0100

    pulse block added to gui and resizing

--- picode/gui_copy2.py
+++ picode/gui_copy2.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 from matplotlib.figure import Figure
 import matplotlib.animation as animation
 import datetime as dt
+import time
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 import math
 import sched
@@ -12,6 +13,10 @@ class GUI():
         self.root = tk.Tk()
         self.root.configure(bg = "black")
         self.root.geometry("800x480")
+        self.root.attributes('-fullscreen', True)
+        self.root.bind('<F11>', self.set_fullscreen)
+        self.root.bind('<Escape>', self.exit_fullscreen)
+        self.root.config(cursor='none')
         plt.style.use('dark_background')
         self.patient_name=patient_name
         self.dob_str=dob_str
@@ -24,33 +29,40 @@ class GUI():
         doctor_info = tk.Label(self.root, text = "Doctor: "+self.doctor_name, fg="white", bg="black")
         doctor_info.grid(row=0, column=1, sticky='W')
         #Heart Rate Digital Initilisation
-        self.hr_info1 = tk.Label(self.root, text = "Heart Rate:", fg="green", bg="black", font=("Arial",14))
-        self.hr_info2 = tk.Label(self.root, text = "--", fg="green", bg="black", font=("Arial", 40))
-        self.hr_info3 = tk.Label(self.root, text = "bpm", fg="green", bg="black", font=("Arial", 14))
+        self.hr_info1 = tk.Label(self.root, text = "Heart Rate:", fg="green", bg="black", font=("Arial",10))
+        self.hr_info2 = tk.Label(self.root, text = "--", fg="green", bg="black", font=("Arial", 25))
+        self.hr_info3 = tk.Label(self.root, text = "bpm", fg="green", bg="black", font=("Arial", 10))
         self.hr_info1.grid(row=1, column=2)
         self.hr_info2.grid(row=2, column=2)
         self.hr_info3.grid(row=3, column=2)
         #SPO2 Digital Initilisaton
-        self.spo2_info1 = tk.Label(self.root, text = "SpO2:", fg="yellow", bg="black", font=("Arial",14))
-        self.spo2_info2 = tk.Label(self.root, text = "--", fg="yellow", bg="black", font=("Arial", 40))
-        self.spo2_info3 = tk.Label(self.root, text = "%", fg="yellow", bg="black", font=("Arial", 14))
+        self.spo2_info1 = tk.Label(self.root, text = "SpO2:", fg="yellow", bg="black", font=("Arial",10))
+        self.spo2_info2 = tk.Label(self.root, text = "--", fg="yellow", bg="black", font=("Arial", 25))
+        self.spo2_info3 = tk.Label(self.root, text = "%", fg="yellow", bg="black", font=("Arial", 10))
         self.spo2_info1.grid(row=4, column=2)
         self.spo2_info2.grid(row=5, column=2)
         self.spo2_info3.grid(row=6, column=2)
         #Temperature Digital Inilisation
-        self.temp_info1 = tk.Label(self.root, text = "Temperature:", fg="red", bg="black", font=("Arial",14))
-        self.temp_info2 = tk.Label(self.root, text = "--", fg="red", bg="black", font=("Arial", 40))
-        self.temp_info3 = tk.Label(self.root, text = "˚C", fg="red", bg="black", font=("Arial", 14))
+        self.temp_info1 = tk.Label(self.root, text = "Temperature:", fg="red", bg="black", font=("Arial",10))
+        self.temp_info2 = tk.Label(self.root, text = "--", fg="red", bg="black", font=("Arial", 25))
+        self.temp_info3 = tk.Label(self.root, text = "˚C", fg="red", bg="black", font=("Arial", 10))
         self.temp_info1.grid(row=7, column=2)
         self.temp_info2.grid(row=8, column=2)
         self.temp_info3.grid(row=9, column=2)
         # Respiration Rate
-        self.resp_info1 = tk.Label(self.root, text = "Respiration Rate:", fg="blue", bg="black", font=("Arial",14))
-        self.resp_info2 = tk.Label(self.root, text = "--", fg="blue", bg="black", font=("Arial", 40))
-        self.resp_info3 = tk.Label(self.root, text = "˚C", fg="blue", bg="black", font=("Arial", 14))
+        self.resp_info1 = tk.Label(self.root, text = "Respiration Rate:", fg="blue", bg="black", font=("Arial",10))
+        self.resp_info2 = tk.Label(self.root, text = "--", fg="blue", bg="black", font=("Arial", 25))
+        self.resp_info3 = tk.Label(self.root, text = "rpm", fg="blue", bg="black", font=("Arial", 10))
         self.resp_info1.grid(row=10, column=2)
         self.resp_info2.grid(row=11, column=2)
         self.resp_info3.grid(row=12, column=2)
+        # Pulse
+        self.pulse_info1 = tk.Label(self.root, text = "Pulse:", fg="purple", bg="black", font=("Arial",10))
+        self.pulse_info2 = tk.Label(self.root, text = "--", fg="purple", bg="black", font=("Arial", 25))
+        self.pulse_info3 = tk.Label(self.root, text = "bpm", fg="purple", bg="black", font=("Arial", 10))
+        self.pulse_info1.grid(row=13, column=2)
+        self.pulse_info2.grid(row=14, column=2)
+        self.pulse_info3.grid(row=15, column=2)
         #Graphs Initilisation
         self.ecg_len=600
         self.ppg_len=200
@@ -64,32 +76,32 @@ class GUI():
         self.y_ppg=[0]*self.ppg_len
         self.y_rr=[0]*self.rr_len
         # ECG Graph pane
-        self.fig_ecg = Figure(figsize=(6.7,1.3), constrained_layout=True)
+        self.fig_ecg = Figure(figsize=(6.7,1.3), tight_layout=True)
         self.ax_ecg = self.fig_ecg.add_subplot(111)
         self.format_axis('e', self.ax_ecg)
         self.graph_ecg = FigureCanvasTkAgg(self.fig_ecg, master=self.root)
-        self.graph_ecg.get_tk_widget().grid(row=1, column=0, columnspan=2, rowspan=4)
+        self.graph_ecg.get_tk_widget().grid(row=1, column=0, columnspan=2, rowspan=5)
         self.ecg_line, =self.ax_ecg.plot(self.x_ecg, self.y_ecg, color="#59eaed")
-        self.ax_ecg.set_ylim([400,700])
+        self.ax_ecg.set_ylim([000,700])
         # PPG Graph pane
-        self.fig_ppg = Figure(figsize=(6.7,1.3), constrained_layout=True)
+        self.fig_ppg = Figure(figsize=(6.7,1.3), tight_layout=True)
         self.ax_ppg = self.fig_ppg.add_subplot(111)
         self.format_axis('p', self.ax_ppg)
         self.graph_ppg = FigureCanvasTkAgg(self.fig_ppg, master=self.root)
-        self.graph_ppg.get_tk_widget().grid(row=5, column=0, columnspan=2, rowspan=4)
+        self.graph_ppg.get_tk_widget().grid(row=6, column=0, columnspan=2, rowspan=5)
         self.ppg_line, =self.ax_ppg.plot(self.x_ppg, self.y_ppg, color="#f51a18")
         self.ax_ppg.set_ylim([0,35000])
         # Respiration Graph pane
-        self.fig_resp = Figure(figsize=(6.7,1.3), constrained_layout=True)
+        self.fig_resp = Figure(figsize=(6.7,1.3), tight_layout=True)
         self.ax_resp = self.fig_resp.add_subplot(111)
         self.format_axis('r', self.ax_resp)
         self.graph = FigureCanvasTkAgg(self.fig_resp, master=self.root)
-        self.graph.get_tk_widget().grid(row=9, column=0, columnspan=2, rowspan=4)
+        self.graph.get_tk_widget().grid(row=11, column=0, columnspan=2, rowspan=5)
         self.rr_line, =self.ax_resp.plot(self.x_rr, self.y_rr, color="#3cd82c")
         self.ax_resp.set_ylim([-2,2])
         # Report issue button
-        self.report_button = tk.Button(self.root, text="REPORT ISSUE", command=self.report, width=70, height=2, fg="white", highlightbackground="black", bg="gray", activebackground="red")
-        self.report_button.grid(row = 13, column=0, columnspan = 2, sticky='E')
+        self.report_button = tk.Button(self.root, text="REPORT ISSUE", command=self.report, width=85, height=2, fg="white", bg="gray", activebackground="gray", activeforeground="white")
+        self.report_button.grid(row = 16, column=0, columnspan = 3)
         #self.ecg_thread=ecgthread
         #self.ppg_thread=ppgthread
         #self.rr_thread=rrthread
@@ -119,8 +131,13 @@ class GUI():
 
     # Set critical flag when report issue button is pressed - send this info to DB
     def report(self):
+        self.report_button.configure(bg="red", activebackground = "red")
+        self.report_button.flash()
         critical = True
         critical_time = dt.datetime.utcnow()
+        self.report_button.configure(bg="gray", activebackground = "gray")
+
+
 
     def updateDigital(self):
         spo2=self.spo2q.get()
@@ -132,9 +149,17 @@ class GUI():
         rr=self.rrq.get()
         self.resp_info2.configure(text=str(rr))
         
-        
-    def animate(self, i, y_ecg, y_ppg, y_resp):
+    def set_fullscreen(self, event=None):
+        self.root.attributes('-fullscreen', True)
+        return "break"
 
+    def exit_fullscreen(self, event=None):
+        self.root.attributes('-fullscreen', False)
+        return "break"
+
+    def animate(self, i, y_ecg, y_ppg, y_resp):
+            
+        '''
         global x_ecg
         y_ecg.extend(self.ecgfiltq.get()) 
         y_ecg = y_ecg[-self.ecg_len:]
@@ -146,6 +171,7 @@ class GUI():
             y_ppg.extend(self.PPGirq.get()) 
         y_ppg = y_ppg[-self.ppg_len:]
         self.ppg_line.set_ydata(y_ppg)
+        
 
         global x_resp
         y_resp.append(math.sin(self.x_rr1/2*math.pi)) 
@@ -155,7 +181,7 @@ class GUI():
             self.x_rr1=0
         self.rr_line.set_ydata(y_resp)
         self.updateDigital()
-
+        '''
 
         return self.ecg_line, self.ppg_line, self.rr_line,
         
