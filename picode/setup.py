e25c175a1f48846d3f400788a9a824c64eeb4fac
Omar Muttawa <OmarMuttawa@dyn3198-58.wlan.ic.ac.uk>
2019-06-24 15:39:16 +0100

    added print statements to setup.py

--- picode/setup.py
+++ picode/setup.py
@@ -1,4 +1,6 @@
 import subprocess
+
+print("Installing libraries:")
 subprocess.call(["sudo","apt-get","install","python3-pyrebase"])
 print("Pyrebase installed")
 subprocess.call(["sudo","apt-get","install","python3-datetime"])
@@ -10,11 +12,16 @@ print("Numpy installed")
 subprocess.call(["sudo","apt-get","install","python3-tk"])
 print("Tkinter installed")
 print("ALL Libraries installed")
+
+print("Configuring screen")
 subprocess.call(["sudo","cp","config.txt", "/boot"])
-print("Screen will rotate")
+print("Screen will rotate after reboot")
+
+'''
+print("Adding Main to boot")
 subprocess.call(["sudo","cp", "lifelinemain.service","/lib/systemd/system/"])
-subprocess.call(["python3","Main.py"])
-#subprocess.call(["sudo chmod", "644", "/lib/systemd/system/lifelinemain.service"])
-#subprocess.call(["sudo systemctl", "daemon-reload"])
-#subprocess.call(["sudo systemctl", "enable", "lifelinemain.service"])
+subprocess.call(["sudo", "chmod", "644", "/lib/systemd/system/lifelinemain.service"])
+subprocess.call(["sudo", "systemctl", "daemon-reload"])
+subprocess.call(["sudo", "systemctl", "enable", "lifelinemain.service"])
 print("Main.py added to boot please reboot!")
+'''
